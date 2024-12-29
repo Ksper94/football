3080632fba93f8e2047a3b0daa512bf04ebd0310
ksper94 <hugostephan94470@gmail.com>
2024-12-29 17:11:21 +0100

    correction

--- football.py
+++ football.py
@@ -16,7 +16,9 @@ if 'authenticated' not in st.session_state:
 # Récupération du token dans l'URL
 params = st.query_params
 st.write(f"Query params: {params}")  # Debug: Afficher tous les paramètres d'URL
-url_token = params.get('token', [None])[0]
+
+# Extraction du token
+url_token = params.get('token', [None])[0] if isinstance(params.get('token'), list) else params.get('token')
 st.write(f"Token extrait de l'URL : {url_token}")  # Debug: Afficher le token extrait
 
 if not st.session_state.authenticated:
