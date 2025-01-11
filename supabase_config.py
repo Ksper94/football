b5795e59c4147d37ed1b80a822c489ec404613c0
ksper94 <hugostephan94470@gmail.com>
2025-01-11 12:00:59 +0100

    nouveau style

---
+++ supabase_config.py
@@ -0,0 +1,10 @@
+# supabase_config.py
+
+from supabase import create_client, Client
+import os
+
+# Initialiser Supabase client
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
